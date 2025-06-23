38afc1e0f76c0bde4588814b0b9c980afcb3548b
JoseLuGeek <joseluisrobleslao10@gmail.com>
2025-06-23 10:14:58 +0000

    GeneradorDominioConMap

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ const pronoun = ['the', 'our'];
 const adj = ['great', 'big'];
 const noun = ['jogger', 'racoon'];
 
-
+// Con bucle for!!
 
 for (let i = 0; i < pronoun.length; i++) {
   for (let j = 0; j < adj.length; j++) {
     for (let k = 0; k < noun.length; k++) {
-      console.log(pronoun[i] + adj[j] + noun[k] + '.com');
+      console.log(pronoun[i] + adj[j] + noun[k] +'.com');
     }
   }
 }
   
+//Con .map!!
+
+pronoun.map((pronombre) => {
+  adj.map((adjetivo) => {
+    noun.map((nombre) => {
+      const resultado = `${pronombre}${adjetivo}${nombre}.com`;
+      console.log(resultado);
+    });
+  });
+});
